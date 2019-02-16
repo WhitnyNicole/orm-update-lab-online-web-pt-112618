e65502c87677d99eea518e3c871e800706dc972d
Whitny Edwards <wne4e2@gmail.com>
2019-02-16 02:27:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,5 +59,6 @@ attr_accessor :id, :name, :grade
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
-  end   
+  end 
+end 
 
